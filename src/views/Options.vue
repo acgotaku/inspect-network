21869ae1dfda31aec2c18ef407bd5b9a8794dfc9
acgotaku <acgotaku311@gmail.com>
2019-06-27 22:38:07 +0900

    add input

--- src/views/Options.vue
+++ src/views/Options.vue
@@ -1,13 +1,49 @@
 <template>
   <div>
     <nav class="header">
-      <h1 class="header-title">Inspect Network</h1>
+      <h1 class="header-title">{{ name }}</h1>
     </nav>
+    <b-container>
+      <div class="add-tab">
+        <b-button variant="success">New Inspect</b-button>
+      </div>
+      <b-card no-body>
+        <b-tabs card>
+          <b-tab title="Tab 1">
+            <b-card-body>
+              <b-form @submit.stop.prevent>
+                <label for="url">Target URL</label>
+                <b-input
+                  type="text"
+                  id="url"
+                  aria-describedby="url-help-block"
+                ></b-input>
+                <b-form-text id="url-help-block">
+                  URL must be start with http or https.
+                </b-form-text>
+              </b-form>
+              <div class=""></div>
+            </b-card-body>
+          </b-tab>
+          <b-tab title="Tab 2">
+            <b-card-text>Tab Contents 2</b-card-text>
+          </b-tab>
+        </b-tabs>
+      </b-card>
+    </b-container>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      name: chrome.i18n.getMessage('appName'),
+      newInspect: chrome.i18n.getMessage('newInspect'),
+      inspects: localStorage.getItem('inspects') || 1
+    };
+  }
+};
 </script>
 
 <style lang="scss">
@@ -23,5 +59,10 @@ export default {};
     font-size: 5rem;
     margin-left: 40px;
   }
+}
+.add-tab {
+  display: flex;
+  justify-content: flex-end;
+  padding: 16px 0;
 }
 </style>
